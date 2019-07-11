4bdc66374e61c7a6b47db4083e5a24de2a77fb18
Christopher Jones <csj@christopherjones.co>
2019-07-11 10:04:52 -0600

    Remove unused imports.

--- src/main/java/org/dataone/bookkeeper/api/Product.java
+++ src/main/java/org/dataone/bookkeeper/api/Product.java
@@ -7,8 +7,6 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Products represent a given offering to be purchased
