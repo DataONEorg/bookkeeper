d724cb8427b21ee7b9a215ff74bf2ffdacd815af
Christopher Jones <csj@christopherjones.co>
2019-07-11 16:19:42 -0600

    Add a FeatureTest
    to ensure the Feature model serializes correctly.

---
+++ src/test/java/org/dataone/bookkeeper/api/FeatureTest.java
@@ -0,0 +1,45 @@
+package org.dataone.bookkeeper.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test the Feature model
+ */
+@DisplayName("Feature model test")
+class FeatureTest {
+    private final static ObjectMapper MAPPER = Jackson.newObjectMapper();
+    private final static String FEATURE_JSON = "fixtures/feature.json";
+    private final static String NAME = "custom_portal";
+    private final static String LABEL = "Branded Portals";
+    private final static String DESCRIPTION = "Showcase your research, data, results, " +
+        "and usage metrics by building a custom web portal.";
+    private final static Quota QUOTA = new Quota(
+        null,
+        "quota",
+        "custom_portal_count",
+        3,
+        3,
+        "portal"
+    );
+
+    /**
+     * Test serialization to JSON
+     */
+    @Test
+    @DisplayName("test Customer model serialization")
+    public void serializesToJSON() throws Exception {
+        // Build the Customer instance
+        final Feature feature = new Feature(NAME, LABEL, DESCRIPTION, QUOTA);
+
+        // Test the Feature instance
+        final String expected = MAPPER.writeValueAsString(
+            MAPPER.readValue(fixture(FEATURE_JSON), Feature.class));
+        assertThat(MAPPER.writeValueAsString(feature)).isEqualTo(expected);
+    }
+}
