1097a044dc95d8e1fed16b6942c2b45ee2d72e9b
Christopher Jones <csj@christopherjones.co>
2019-07-17 14:56:45 -0600

    Update Quota test to use a customerId
    The customerId field can be null, and we want to strip null properties
    from the JSON when serializing, so configure the ObjectMapper to do so.

--- src/test/java/org/dataone/bookkeeper/api/QuotaTest.java
+++ src/test/java/org/dataone/bookkeeper/api/QuotaTest.java
@@ -1,6 +1,7 @@
 package org.dataone.bookkeeper.api;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.jackson.Jackson;
 import org.junit.jupiter.api.DisplayName;
@@ -18,9 +19,10 @@ class QuotaTest {
     private static final Long ID = 1L;
     private static final String OBJECT = "quota";
     private static final String NAME = "custom_portal_count";
-    private static final int SOFTLIMIT = 3;
-    private static final int HARDLIMIT = 3;
+    private static final Long SOFTLIMIT = 3L;
+    private static final Long HARDLIMIT = 3L;
     private static final String UNIT =  "portal";
+    private static final Long CUSTOMER_ID = null;
 
     /**
      * Test serialization to JSON
@@ -29,9 +31,10 @@ class QuotaTest {
     @DisplayName("Test Quota model serialization")
     public void serializesToJSON() throws Exception {
         // Build the Customer instance
-        final Quota quota = new Quota(ID, OBJECT, NAME, SOFTLIMIT, HARDLIMIT, UNIT);
-
+        final Quota quota = new Quota(ID, OBJECT, NAME, SOFTLIMIT, HARDLIMIT, UNIT, CUSTOMER_ID);
+        quota.getCustomerId();
         // Test the Customer instance
+        MAPPER.setSerializationInclusion(Include.NON_NULL);
         final String expected = MAPPER.writeValueAsString(
             MAPPER.readValue(fixture("fixtures/quota.json"), Quota.class));
         assertThat(MAPPER.writeValueAsString(quota)).isEqualTo(expected);
