5bb30abe157f49475a8adaf95c5af129ee8dcd04
Christopher Jones <csj@christopherjones.co>
2019-07-18 16:27:13 -0600

    Convert standard quotas to megabyte scale.

--- src/main/resources/db/migrations/V1.2__Insert_Standard_Quotas.sql
+++ src/main/resources/db/migrations/V1.2__Insert_Standard_Quotas.sql
@@ -2,44 +2,44 @@
 
 -- 5GB standard account limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 5368709120, 5905580032, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 5120, 5632, 'megabyte', NULL);
 
 -- 1 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 1099511627776, 1209462790554, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 1048576, 1153433, 'megabyte', NULL);
 
 -- 2 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 2199023255552, 2308974418330, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 2097152, 2202009, 'megabyte', NULL);
 
 -- 3 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 3298534883328, 3408486046106, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 3145728, 3250585, 'megabyte', NULL);
 
 -- 4 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 4398046511104, 4507997673882, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 4194304, 4299161, 'megabyte', NULL);
 
 -- 5 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 5497558138880, 5607509301658, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 5242880, 5347737, 'megabyte', NULL);
 
 -- 6 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 6597069766656, 6707020929434, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 6291456, 6396313, 'megabyte', NULL);
 
 -- 7 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 7696581394432, 7806532557210, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 7340032, 7444889, 'megabyte', NULL);
 
 -- 8 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 8796093022208, 8906044184986, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 8388608, 8493465, 'megabyte', NULL);
 
 -- 9 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 9895604649984, 10005555812761, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 9437184, 9542041, 'megabyte', NULL);
 
 -- 10 TB hosted repository or archival storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, unit, customerId)
-    VALUES ('quota', 'custom_portal_quota', 10995116277760, 11105067440538, 'byte', NULL);
+    VALUES ('quota', 'custom_portal_quota', 10485760, 10590617, 'megabyte', NULL);
