a2a0469227df2ce96fc2386ea10421d0da107306
Christopher Jones <csj@christopherjones.co>
2019-07-20 09:22:57 -0600

    Add a QuotaDAO.insert() method
    Insert a quota by passing in an instance and binding the bean fields.
    Rename some methods with typos.

--- src/main/java/org/dataone/bookkeeper/jdbi/QuotaDAO.java
+++ src/main/java/org/dataone/bookkeeper/jdbi/QuotaDAO.java
@@ -3,6 +3,7 @@ package org.dataone.bookkeeper.jdbi;
 import org.dataone.bookkeeper.api.Quota;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -31,7 +32,7 @@ public interface QuotaDAO {
         "q.id, q.object, q.name, q.softLimit, q.hardLimit, q.unit, q.customerId " +
         "FROM quotas q " +
         "WHERE q.id = :id")
-    List<Quota> findQuotaById(@Bind("id") Integer id);
+    List<Quota> findQuotasById(@Bind("id") Integer id);
 
     /**
      * Find quotas by customer identifier.
@@ -44,6 +45,17 @@ public interface QuotaDAO {
         "FROM quotas q " +
         "WHERE q.customerId = :customerId")
     List<Quota> findQuotasByCustomerId(@Bind("customerId") Integer customerId);
+
+
+    /**
+     * Insert a quota with a given Quota instance
+     * @param quota
+     */
+    @SqlUpdate("INSERT INTO quotas " +
+        "(object, name, softLimit, hardLimit, unit, customerId) " +
+        "VALUES " +
+        "(:object, :name, :softLimit, :hardLimit, :unit, :customerId)")
+    void insert(@BindBean Quota quota);
 
     /**
      * Insert a quota with an optionally null customer ID
