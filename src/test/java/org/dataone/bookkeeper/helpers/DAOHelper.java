bb746896ea31c1760e27923f10fb3368a5b45dbc
Christopher Jones <csj@christopherjones.co>
2019-07-26 16:05:02 -0600

    Add a delegate class for general DAO testing
    For now, DAOHelper provides the getRandomId() method so all
    tests can create objects with unique ids.

---
+++ src/test/java/org/dataone/bookkeeper/helpers/DAOHelper.java
@@ -0,0 +1,19 @@
+package org.dataone.bookkeeper.helpers;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * A delegate class with helper methods for all testing
+ */
+public class DAOHelper {
+
+    /**
+     * Generate a random Integer for use as test case identifiers
+     * @return
+     */
+    public static Integer getRandomId() {
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 100000000);
+        return new Integer(randomInt);
+    }
+
+}
