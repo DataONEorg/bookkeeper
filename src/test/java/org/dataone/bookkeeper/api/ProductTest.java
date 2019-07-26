e200fd05c4b2d8d42ae1fe84ed955d223d0b0ff9
Christopher Jones <csj@christopherjones.co>
2019-07-26 15:08:57 -0600

    Refactor the ProductTest to use the ProductHelper.

--- src/test/java/org/dataone/bookkeeper/api/ProductTest.java
+++ src/test/java/org/dataone/bookkeeper/api/ProductTest.java
@@ -2,8 +2,8 @@ package org.dataone.bookkeeper.api;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.dropwizard.jackson.Jackson;
+import org.dataone.bookkeeper.helpers.ProductHelper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,83 +21,6 @@ public class ProductTest {
     }
     private final static String PRODUCT_JSON = "fixtures/product.json";
 
-    // Set fields for the Product instance to test
-    private final static Integer ID = 1;
-    private final static String OBJECT = "product";
-    private final static boolean ACTIVE = true;
-    private final static String NAME = "Organization";
-    private final static String CAPTION = "Small institutions or groups";
-    private final static String DESCRIPTION = "Create multiple portals for your work and projects. Help others understand and access your data.";
-    private final static Integer CREATED = 1559768309;
-    private final static String STATEMENT_DESCRIPTOR = "DataONE Membership Plan - Organization";
-    private final static String TYPE = "service";
-    private final static String UNIT_LABEL = "membership";
-    private final static String URL = "https://dataone.org/memberships/organization";
-    // Build up a feature list
-    private final static Feature FEATURE1 = new Feature();
-    private final static Feature FEATURE2 = new Feature();
-    private final static Feature FEATURE3 = new Feature();
-    private final static Feature FEATURE4 = new Feature();
-    private final static Feature FEATURE5 = new Feature();
-    private final static Feature FEATURE6 = new Feature();
-
-    // Build a quota for feature 1
-    private static final  Integer QUOTA_ID = 1;
-    private static final String QUOTA_TYPE = "quota";
-    private static final String QUOTA_NAME = "custom_portal_count";
-    private static final  Integer QUOTA_SOFT_LIMIT = 3;
-    private static final  Integer QUOTA_HARD_LIMIT = 3;
-    private static final String QUOTA_UNIT = "portal";
-    private static final  Integer QUOTA_CUSTOMER_ID = null;
-
-    private static final Quota QUOTA = new Quota(QUOTA_ID, QUOTA_TYPE, QUOTA_NAME,
-        QUOTA_SOFT_LIMIT, QUOTA_HARD_LIMIT, QUOTA_UNIT, QUOTA_CUSTOMER_ID);
-
-    static {
-        FEATURE1.setName("custom_portal");
-        FEATURE1.setLabel("Branded Portals");
-        FEATURE1.setDescription("Showcase your research, data, results, and usage metrics by building a custom web portal.");
-        FEATURE1.setQuota(QUOTA);
-
-        FEATURE2.setName("custom_search_filters");
-        FEATURE2.setLabel("Custom Search Filters");
-        FEATURE2.setDescription("Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science.");
-        FEATURE2.setQuota(null);
-
-        FEATURE3.setName("fair_data_assessment");
-        FEATURE3.setLabel("FAIR Data Assessments");
-        FEATURE3.setDescription("Access quality metric reports using the FAIR data suite of checks.");
-        FEATURE3.setQuota(null);
-
-        FEATURE4.setName("custom_quality_metrics");
-        FEATURE4.setLabel("Custom Quality Metrics");
-        FEATURE4.setDescription("Create a suite of custom quality metadata checks specific to your datasets.");
-        FEATURE4.setQuota(null);
-
-        FEATURE5.setName("aggregated_metrics");
-        FEATURE5.setLabel("Aggregated Metrics");
-        FEATURE5.setDescription("Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations.");
-        FEATURE5.setQuota(null);
-
-        FEATURE6.setName("dataone_voting_member");
-        FEATURE6.setLabel("DataONE Voting Member");
-        FEATURE6.setDescription("Vote on the direction and priorities at DataONE Community meetings.");
-        FEATURE6.setQuota(null);
-
-    }
-
-    // Add the feature list to the metadata object
-    private final static ObjectNode METADATA = MAPPER.createObjectNode();
-    static {
-        // ArrayNode features = MAPPER.createArrayNode()
-        METADATA.putArray("features")
-            .add(MAPPER.convertValue(FEATURE1, ObjectNode.class))
-            .add(MAPPER.convertValue(FEATURE2, ObjectNode.class))
-            .add(MAPPER.convertValue(FEATURE3, ObjectNode.class))
-            .add(MAPPER.convertValue(FEATURE4, ObjectNode.class))
-            .add(MAPPER.convertValue(FEATURE5, ObjectNode.class))
-            .add(MAPPER.convertValue(FEATURE6, ObjectNode.class));
-   }
     /**
      * Test serialization to JSON
      * @throws Exception
@@ -106,14 +29,13 @@ public class ProductTest {
     @DisplayName("Test Product model serialization")
     public void serializesToJSON() throws Exception {
         // Build a Product instance
-        final Product product = new Product(ID, OBJECT, ACTIVE, NAME,
-            CAPTION, DESCRIPTION, CREATED, STATEMENT_DESCRIPTOR, TYPE,
-            UNIT_LABEL, URL, METADATA);
+        final Product product = ProductHelper.createTestProduct(1);
+        final String actual = MAPPER.writeValueAsString(product);
 
         // Test the Product instance
         final String expected = MAPPER.writeValueAsString(
             MAPPER.readValue(fixture("fixtures/product.json"), Product.class));
-        assertThat(MAPPER.writeValueAsString(product)).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     /**
@@ -123,9 +45,7 @@ public class ProductTest {
     @DisplayName("Test Product model deserialization")
     public void deserializesFromJSON() throws Exception {
         // Build the Product instance
-        final Product product = new Product(ID, OBJECT, ACTIVE, NAME,
-            CAPTION, DESCRIPTION, CREATED, STATEMENT_DESCRIPTOR, TYPE,
-            UNIT_LABEL, URL, METADATA);
+        final Product product = ProductHelper.createTestProduct(1);
 
         // Test the Product instance
         final Product deserializedProduct =
