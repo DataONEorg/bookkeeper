63a5aed2fd5f55f38c095750d13403c508f22abd
Christopher Jones <csj@christopherjones.co>
2019-07-31 09:43:01 -0600

    Update the CustomerMapper to use correct field names.
    We can use the Quota BeanMapper directly if we prefix the Customer
    fields with "c_", since we need a custom mapper for customers anyway.
    Also refactor the quotas filed to be a List type.

--- src/main/java/org/dataone/bookkeeper/jdbi/mappers/CustomerMapper.java
+++ src/main/java/org/dataone/bookkeeper/jdbi/mappers/CustomerMapper.java
@@ -5,13 +5,15 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.dropwizard.jackson.Jackson;
 import org.dataone.bookkeeper.api.Address;
 import org.dataone.bookkeeper.api.Customer;
+import org.dataone.bookkeeper.api.Quota;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
-import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Maps customer rows to Customer instances. We use this since the JSON
@@ -35,30 +37,48 @@ public class CustomerMapper implements RowMapper<Customer> {
      * @throws SQLException if anything goes wrong go ahead and let this percolate; Jdbi will handle it
      */
     @Override
-    @RegisterBeanMapper(value = Customer.class, prefix = "c") // TODO: Does this work?
     public Customer map(ResultSet rs, StatementContext ctx) throws SQLException {
-        Customer customer;
+        Customer customer = null;
+        Quota quota = null;
+        List<Quota> quotas = new LinkedList<Quota>();
         ObjectMapper mapper = Jackson.newObjectMapper();
+
         try {
+
+            // If we have quota fields in the resultset, build and add a quota
+            if (new Integer(rs.getInt("id")) != null ) {
+                quota = new Quota(
+                    new Integer(rs.getInt("id")),
+                    rs.getString("object"),
+                    rs.getString("name"),
+                    new Integer(rs.getInt("softLimit")),
+                    new Integer(rs.getInt("hardLimit")),
+                    rs.getString("unit"),
+                    new Integer(rs.getInt("c_id"))
+                    );
+                quotas.add(quota);
+            }
+
+            // Create a customer instance from the resultset
             customer = new Customer(
-                new Integer(rs.getInt("id")),
-                rs.getString("object"),
-                rs.getString("orcid"),
-                new Integer(rs.getInt("balance")),
-                (Address) mapper.readValue(rs.getString("address"), Address.class),
-                new Integer(rs.getInt("created")),
-                rs.getString("currency"),
-                rs.getBoolean("delinquent"),
-                rs.getString("description"),
-                (ObjectNode) mapper.readTree(rs.getString("discount")),
-                rs.getString("email"),
-                rs.getString("invoiceprefix"),
-                (ObjectNode) mapper.readTree(rs.getString("invoicesettings")),
-                (ObjectNode) mapper.readTree(rs.getString("metadata")),
-                rs.getString("givenname"),
-                rs.getString("surname"),
-                rs.getString("phone"),
-                (ObjectNode) mapper.readTree(rs.getString("quotas"))
+                new Integer(rs.getInt("c_id")),
+                rs.getString("c_object"),
+                rs.getString("c_orcid"),
+                new Integer(rs.getInt("c_balance")),
+                mapper.readValue(rs.getString("c_address"), Address.class),
+                new Integer(rs.getInt("c_created")),
+                rs.getString("c_currency"),
+                rs.getBoolean("c_delinquent"),
+                rs.getString("c_description"),
+                (ObjectNode) mapper.readTree(rs.getString("c_discount")),
+                rs.getString("c_email"),
+                rs.getString("c_invoicePrefix"),
+                (ObjectNode) mapper.readTree(rs.getString("c_invoiceSettings")),
+                (ObjectNode) mapper.readTree(rs.getString("c_metadata")),
+                rs.getString("c_givenName"),
+                rs.getString("c_surName"),
+                rs.getString("c_phone"),
+                quotas
             );
         } catch (IOException e) {
             throw new SQLException(e);
