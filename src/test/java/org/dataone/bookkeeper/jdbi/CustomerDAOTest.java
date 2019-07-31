2563325a2ae5a31750157da85ab33d65a01da0f2
Christopher Jones <csj@christopherjones.co>
2019-07-31 09:49:41 -0600

    Update the CustomerDAOTest to test for quotas.
    Add the testListCustomersWithPortalQuota() test, which inserts a
    customer with quotas, and tests that the quotas are returned correctly
    given the SQL join across the customers and quotas tables.

--- src/test/java/org/dataone/bookkeeper/jdbi/CustomerDAOTest.java
+++ src/test/java/org/dataone/bookkeeper/jdbi/CustomerDAOTest.java
@@ -1,9 +1,15 @@
 package org.dataone.bookkeeper.jdbi;
 
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.dropwizard.jackson.Jackson;
 import org.dataone.bookkeeper.BaseTestCase;
+import org.dataone.bookkeeper.api.Customer;
+import org.dataone.bookkeeper.api.Quota;
 import org.dataone.bookkeeper.helpers.CustomerHelper;
 import org.dataone.bookkeeper.helpers.DAOHelper;
+import org.dataone.bookkeeper.helpers.QuotaHelper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -50,17 +56,49 @@ public class CustomerDAOTest extends BaseTestCase {
         }
     }
 
+    /**
+     * Test listing customers with empty quotas
+     */
     @Test
     @DisplayName("Test listing the customers")
-    public void testListProducts() {
+    public void testListCustomers() {
         // Insert a new product
         Integer customerId = null;
         try {
             customerId = CustomerHelper.insertTestCustomer(DAOHelper.getRandomId());
+            this.customerIds.add(customerId);
+
         } catch (SQLException e) {
             fail(e);
         }
-        this.customerIds.add(customerId);
         assertTrue(customerDAO.listCustomers().size() >= 1);
+    }
+
+    @Test
+    @DisplayName("Test listing customers with associated portal quotas")
+    public void testListCustomersWithPortalQuota() {
+
+        try {
+            // Insert a customer
+            final Integer customerId = CustomerHelper.insertTestCustomer(DAOHelper.getRandomId());
+            this.customerIds.add(customerId);
+
+            // Insert a portal quota for the customer
+            final Integer quotaId =
+                QuotaHelper.insertTestQuotaWithCustomer(DAOHelper.getRandomId(), customerId);
+                customerDAO.listCustomers().forEach(customer -> {
+                    customer.getQuotas()
+                        .forEach(quota -> {
+                            assertTrue(quota.getId().equals(quotaId));
+                            assertTrue(quota.getCustomerId().equals(customerId));
+                        });
+                });
+        } catch (SQLException e) {
+            fail(e);
+        }
+
+        // Add a portal quota for the customer
+
+
     }
 }
