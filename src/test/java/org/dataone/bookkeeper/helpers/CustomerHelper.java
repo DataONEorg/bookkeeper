f8897cb248c521af447ea943adf58b723253f583
Christopher Jones <csj@christopherjones.co>
2019-07-31 15:41:59 -0600

    Add new static methods to the CustomerHelper delegate
    Add a createCustomer() method that takes a customer id and returns a
    Customer instance.
    
    Add an insertTestCustomer() method that takes a Customer instance and
    inserts it into the database, returning the Customer instance.

--- src/test/java/org/dataone/bookkeeper/helpers/CustomerHelper.java
+++ src/test/java/org/dataone/bookkeeper/helpers/CustomerHelper.java
@@ -1,8 +1,14 @@
 package org.dataone.bookkeeper.helpers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.dropwizard.jackson.Jackson;
 import org.dataone.bookkeeper.BaseTestCase;
+import org.dataone.bookkeeper.api.Address;
+import org.dataone.bookkeeper.api.Customer;
+import org.dataone.bookkeeper.api.Quota;
 
 import java.sql.SQLException;
+import java.util.LinkedList;
 
 /**
  * A delegate class with helper methods for manipulating the customers table for testing
@@ -45,6 +51,79 @@ public class CustomerHelper {
         return customerId;
     }
 
+    /**
+     * Insert a test customer given the Customer instance
+     * @param customer
+     * @return customer the inserted customer
+     * @throws SQLException
+     * @throws JsonProcessingException
+     */
+    public static Customer insertTestCustomer(Customer customer)
+        throws SQLException, JsonProcessingException {
+        BaseTestCase.dbi.useHandle(handle ->
+            handle.execute("INSERT INTO customers " +
+                "(id, object, orcid, balance, address, created, currency, delinquent, " +
+                "description, discount, email, invoicePrefix, invoiceSettings, " +
+                "metadata, givenName, surName, phone) " +
+                "VALUES (?, ?, ?, ?, ?::json, to_timestamp(?), ?, ?, " +
+                "?, ?::json, ?, ?, ?::json, ?::json, ?, ?, ?)",
+                customer.getId(),
+                customer.getObject(),
+                customer.getOrcid(),
+                customer.getBalance(),
+                Jackson.newObjectMapper().writeValueAsString(customer.getAddress()),
+                customer.getCreated(),
+                customer.getCurrency(),
+                customer.isDelinquent(),
+                customer.getDescription(),
+                Jackson.newObjectMapper().writeValueAsString(customer.getDiscount()),
+                customer.getEmail(),
+                customer.getInvoicePrefix(),
+                Jackson.newObjectMapper().writeValueAsString(customer.getInvoiceSettings()),
+                Jackson.newObjectMapper().writeValueAsString(customer.getMetadata()),
+                customer.getGivenName(),
+                customer.getSurName(),
+                customer.getPhone()
+                )
+        );
+        return customer;
+    }
+
+    /**
+     * Create a customer for unit tests
+     * @param customerId the customer id
+     * @return customer the Customer instance
+     */
+    public static Customer createCustomer(Integer customerId) {
+        Customer customer = new Customer(
+            customerId,
+            "customer",
+            "http://orcid.org/0000-0002-8121-2341",
+            0,
+            new Address(
+                "735 State Street",
+                "Suite 300",
+                "Santa Barbara",
+                "CA",
+                "93106",
+                "USA"
+            ),
+            1562866734,
+            "USD",
+            false,
+            "",
+            Jackson.newObjectMapper().createObjectNode(),
+            "cjones@nceas.ucsb.edu",
+            "",
+            Jackson.newObjectMapper().createObjectNode(),
+            Jackson.newObjectMapper().createObjectNode(),
+            "Christopher",
+            "Jones",
+            "805-893-2500",
+            new LinkedList<Quota>()
+        );
+        return customer;
+    }
     /**
      * Remove a test customer given its id
      * @param customerId
