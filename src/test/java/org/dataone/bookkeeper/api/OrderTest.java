9f7fdeaf119fe770bfe4d61b0c0e8398c93eddba
Christopher Jones <csj@christopherjones.co>
2019-08-02 12:44:48 -0600

    Add a QuotaTest unit test
    Test that Order serialization works.

---
+++ src/test/java/org/dataone/bookkeeper/api/OrderTest.java
@@ -0,0 +1,41 @@
+package org.dataone.bookkeeper.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.dataone.bookkeeper.helpers.OrderHelper.*;
+
+/**
+ * Test the Order model
+ */
+public class OrderTest {
+    private final static ObjectMapper MAPPER = Jackson.newObjectMapper();
+    static {
+        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    }
+    private final static String ORDER_JSON = "fixtures/order.json";
+
+    /**
+     * Test serialization to JSON
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Test Order model serialization")
+    public void serializesToJSON() throws Exception {
+        // Build a Order instance
+        final Order order = createTestOrder(1, 2, 3, 4);
+        final String actual = MAPPER.writeValueAsString(order);
+
+        // Test the Order instance
+        final String expected = MAPPER.writeValueAsString(
+            MAPPER.readValue(fixture(ORDER_JSON), Order.class));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
