2c23d367583fc434f4acc5420ab8b142e3ddd312
Christopher Jones <csj@christopherjones.co>
2019-08-03 14:08:32 -0600

    Add an OrderDAOTest unit test to test the OrderDAO interface.
    Include set up and tear down methods that initialize the OrderDAO
    before each test and remove content from the database after each test.
    Add a testListOrders() unit test to exercise the listOrders() method.

---
+++ src/test/java/org/dataone/bookkeeper/jdbi/OrderDAOTest.java
@@ -0,0 +1,86 @@
+package org.dataone.bookkeeper.jdbi;
+
+import org.dataone.bookkeeper.BaseTestCase;
+import org.dataone.bookkeeper.helpers.CustomerHelper;
+import org.dataone.bookkeeper.helpers.DAOHelper;
+import org.dataone.bookkeeper.helpers.OrderHelper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+/**
+ * Test the order data access object
+ */
+public class OrderDAOTest extends BaseTestCase {
+
+    // The OrderDAO to test
+    private OrderDAO orderDAO;
+
+    // A list of order ids used in testing
+    private List<Integer> orderIds = new ArrayList<Integer>();
+
+    // A list of customer ids used in testing
+    private List<Integer> customerIds = new ArrayList<Integer>();
+
+    /**
+     * Set up the DAO for testing
+     */
+    @BeforeEach
+    public void init() {
+        orderDAO = dbi.onDemand(OrderDAO.class);
+    }
+
+    /**
+     * Tear down resources
+     */
+    @AfterEach
+    public void tearDown() {
+        // Remove test order entries
+        for (Integer orderId : this.orderIds) {
+            try {
+                OrderHelper.removeTestOrder(orderId);
+            } catch (SQLException e) {
+                fail(e);
+            }
+        }
+
+        // Remove test customer entries
+        for (Integer customerId : this.customerIds) {
+            try {
+                CustomerHelper.removeTestCustomer(customerId);
+            } catch (SQLException e) {
+                fail(e);
+            }
+        }
+    }
+
+    /**
+     * Test listing the orders
+     */
+    @Test
+    @DisplayName("Test list the orders")
+    public void testListOrders() {
+        try {
+            // Insert a new customer
+            Integer customerId = CustomerHelper.insertTestCustomer(DAOHelper.getRandomId());
+            this.customerIds.add(customerId);
+
+            // Insert a new order for the customer
+            Integer orderId = OrderHelper.insertTestOrder(DAOHelper.getRandomId(), customerId);
+
+            // Test listing the orders
+            assertTrue(orderDAO.listOrders().size() >= 1);
+        } catch (SQLException e) {
+            fail(e);
+        }
+    }
+}
