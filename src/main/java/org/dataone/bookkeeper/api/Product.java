e66dda7e36b04ae585f4d15b775ff790741db78f
Christopher Jones <csj@christopherjones.co>
2019-09-30 16:07:02 -0600

    Minor change to only serialize non-null values in JSON.

--- src/main/java/org/dataone/bookkeeper/api/Product.java
+++ src/main/java/org/dataone/bookkeeper/api/Product.java
@@ -21,6 +21,7 @@
 
 package org.dataone.bookkeeper.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -37,6 +38,7 @@ import java.util.Objects;
 /**
  * Products represent a given offering to be purchased
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product {
 
     /* The product id */
