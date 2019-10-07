b2ec87362a43c5f61eb2ed524a53588579236ff5
Christopher Jones <csj@christopherjones.co>
2019-10-07 14:04:27 -0600

    Reorganize SQL DDL scripts to add subscriptions
    Add a subscriptions table and create a foreign key constraint from
    the quotas table to associate quotas with a subscription.
    Also create a foreign key constraint on the customers
    table id field to associate a subscription with a customer.
    Reorganize the scripts to add the subscriptions table in the correct order.

--- src/main/resources/db/migrations/V1.1__Create_Quotas_Table.sql
+++ src/main/resources/db/migrations/V1.1__Create_Quotas_Table.sql
@@ -12,13 +12,13 @@ CREATE TABLE IF NOT EXISTS quotas (
     hardLimit bigint NOT NULL,
     usage bigint,
     unit text NOT NULL,
-    customerId integer,
+    subscriptionId integer,
     subject text
 );
 ALTER SEQUENCE quotas_id_seq OWNED BY quotas.id;
 
 CREATE INDEX quotas_name_idx ON quotas USING btree(name);
-CREATE INDEX quotas_customerId_idx ON quotas USING btree(customerId);
+CREATE INDEX quotas_subscriptionId_idx ON quotas USING btree(subscriptionId);
 CREATE INDEX quotas_subject_idx ON quotas USING btree(subject);
 
 COMMENT ON TABLE quotas IS 'Quotas limiting resources for products per customer. Quotas may be associated with products or customers by their ids.';
@@ -29,5 +29,5 @@ COMMENT ON COLUMN quotas.softLimit IS 'The soft limit of the resource.';
 COMMENT ON COLUMN quotas.hardLimit IS 'The hard limit of the resource.';
 COMMENT ON COLUMN quotas.usage IS 'The current observed usage of the quota.';
 COMMENT ON COLUMN quotas.unit IS 'The named unit of the quota.';
-COMMENT ON COLUMN quotas.customerId IS 'The customer id to which the quota is applied.';
+COMMENT ON COLUMN quotas.subscriptionId IS 'The subscription id to which the quota is applied.';
 COMMENT ON COLUMN quotas.subject IS 'The subject id to which the quota is applied.';
--- src/main/resources/db/migrations/V1.4__Alter_Quotas_Add_Customer_FK.sql
+++
@@ -1,7 +0,0 @@
---- Note that this is completed in k8s via the Dockerfile
---- or via a shell script in non-k8s development envs
-
---- Add the customer id foreign key constraint
-ALTER TABLE quotas
-    ADD CONSTRAINT quotas_customer_id_fk
-    FOREIGN KEY (customerId) REFERENCES customers (id) ON DELETE CASCADE;
