1b116739f3007f30fbdb6d02557f5738234216d7
Christopher Jones <csj@christopherjones.co>
2019-10-08 14:45:20 -0600

    Update subscription SQL DDL based on the simplified model
    Use the simplified subscription model to create the subscriptions table.

--- src/main/resources/db/migrations/V1.3__Create_Subscriptions_Table.sql
+++ src/main/resources/db/migrations/V1.3__Create_Subscriptions_Table.sql
@@ -7,17 +7,17 @@ CREATE SEQUENCE subscriptions_id_seq;
 CREATE TABLE IF NOT EXISTS subscriptions (
     id integer DEFAULT nextval('subscriptions_id_seq') PRIMARY KEY,
     object text NOT NULL,
-    billingCycleAnchor timestamp with time zone,
+    --- billingCycleAnchor timestamp with time zone,
     canceledAt timestamp with time zone,
     collectionMethod text NOT NULL,
     created timestamp with time zone,
-    currentPeriodEnd timestamp with time zone,
-    currentPeriodStart timestamp with time zone,
+    --- currentPeriodEnd timestamp with time zone,
+    --- currentPeriodStart timestamp with time zone,
     customerId integer NOT NULL,
-    daysUntilDue integer,
+    --- daysUntilDue integer,
     discount json,
-    endedAt timestamp with time zone,
-    latestInvoice json,
+    --- endedAt timestamp with time zone,
+    --- latestInvoice json,
     metadata json,
     productId integer NOT NULL,
     quantity integer,
@@ -33,17 +33,17 @@ CREATE INDEX subscriptions_status_idx ON subscriptions USING btree(status);
 COMMENT ON TABLE  subscriptions IS 'Subscriptions of products and their quotas by customers.';
 COMMENT ON COLUMN subscriptions.id IS 'The unique subscription identifier.';
 COMMENT ON COLUMN subscriptions.object IS 'The serialized object type, set to "subscription".';
-COMMENT ON COLUMN subscriptions.billingCycleAnchor IS 'The timestamp used to calculate subscription billing cycles.';
+--- COMMENT ON COLUMN subscriptions.billingCycleAnchor IS 'The timestamp used to calculate subscription billing cycles.';
 COMMENT ON COLUMN subscriptions.canceledAt IS 'The timestamp when the subscription was canceled.';
 COMMENT ON COLUMN subscriptions.collectionMethod IS 'The payment collection (charge_automatically, or send_invoice).';
 COMMENT ON COLUMN subscriptions.created IS 'The timestamp when the subscription was created.';
-COMMENT ON COLUMN subscriptions.currentPeriodEnd IS 'The timestamp when the current subscription period ends.';
-COMMENT ON COLUMN subscriptions.currentPeriodStart IS 'The timestamp when the current subscription period ends.';
+--- COMMENT ON COLUMN subscriptions.currentPeriodEnd IS 'The timestamp when the current subscription period ends.';
+--- COMMENT ON COLUMN subscriptions.currentPeriodStart IS 'The timestamp when the current subscription period ends.';
 COMMENT ON COLUMN subscriptions.customerId IS 'The customer identifier.';
-COMMENT ON COLUMN subscriptions.daysUntilDue IS 'The number of days until the subscription payment is due.';
-COMMENT ON COLUMN subscriptions.discount IS 'The discount JSON object applied to the subscription.';
-COMMENT ON COLUMN subscriptions.endedAt IS 'The timestamp when the subscription ended.';
-COMMENT ON COLUMN subscriptions.latestInvoice IS 'The latest invoice JSON object for the subscription.';
+--- COMMENT ON COLUMN subscriptions.daysUntilDue IS 'The number of days until the subscription payment is due.';
+--- COMMENT ON COLUMN subscriptions.discount IS 'The discount JSON object applied to the subscription.';
+--- COMMENT ON COLUMN subscriptions.endedAt IS 'The timestamp when the subscription ended.';
+--- COMMENT ON COLUMN subscriptions.latestInvoice IS 'The latest invoice JSON object for the subscription.';
 COMMENT ON COLUMN subscriptions.metadata IS 'The subscription metadata, stored as a JSON object (undefined as of yet)';
 COMMENT ON COLUMN subscriptions.productId IS 'The subscribed product identifier.';
 COMMENT ON COLUMN subscriptions.quantity IS 'The quantity of the subscribed product.';
