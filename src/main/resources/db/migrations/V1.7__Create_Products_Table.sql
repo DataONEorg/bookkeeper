860253911bea381e0484cc77f27bd6b59d753356
Christopher Jones <csj@christopherjones.co>
2019-10-14 07:56:58 -0600

    Update SQL scripts to include product amount, currency, and interval.
    Reflect the changes in the Product model in the products table in the
    database, and add column definitions.  Include the values in the
    standard product entries.

--- src/main/resources/db/migrations/V1.7__Create_Products_Table.sql
+++ src/main/resources/db/migrations/V1.7__Create_Products_Table.sql
@@ -8,9 +8,12 @@ CREATE TABLE IF NOT EXISTS products (
     id integer DEFAULT nextval('products_id_seq') PRIMARY KEY,
     object text NOT NULL,
     active boolean,
+    amount integer NOT NULL,
     name text,
     caption text,
+    currency text NOT NULL,
     description text,
+    interval text NOT NULL,
     created timestamp with time zone,
     statementDescriptor text,
     type text,
@@ -29,15 +32,15 @@ COMMENT ON TABLE  products IS 'Products that are offered as services.';
 COMMENT ON COLUMN products.id IS 'The unique product identifier';
 COMMENT ON COLUMN products.object IS 'The serialized object type, set to "product"';
 COMMENT ON COLUMN products.active IS 'The active status for the product, true or false.';
+COMMENT ON COLUMN products.amount IS 'The cost amount for the product, in pence of the currency';
 COMMENT ON COLUMN products.name IS 'The product name.';
 COMMENT ON COLUMN products.caption IS 'The product caption, used for display.';
+COMMENT ON COLUMN products.currency IS 'The product currency as a 3-letter ISO currency code.';
 COMMENT ON COLUMN products.description IS 'The product general description.';
+COMMENT ON COLUMN products.interval IS 'The product payment interval (day, week, month, year).';
 COMMENT ON COLUMN products.created IS 'The product creation date.';
 COMMENT ON COLUMN products.statementDescriptor IS 'The product statement descriptor used on charge statements.';
 COMMENT ON COLUMN products.type IS 'The product type, either service or good.';
 COMMENT ON COLUMN products.unitLabel IS 'The product unit label.';
 COMMENT ON COLUMN products.url IS 'The product URL used for more information.';
 COMMENT ON COLUMN products.metadata IS 'The product metadata JSON object, currently a feature list.';
-
-
-
---
+++ src/main/resources/db/migrations/V1.8__Insert_Standard_Products.sql
@@ -0,0 +1,88 @@
+--- Insert products
+
+--- Individual Subscription
+INSERT INTO products (object, active, amount, name, caption, currency, description, interval,
+    created, statementDescriptor, type, unitLabel, url, metadata)
+    VALUES (
+    'product',
+    true,
+    30000,
+    'Individual',
+    'Faculty or research lab',
+    'USD',
+    'Create a customized portal for your work and projects. Help others understand and access your data.',
+    'year',
+    now(),
+    'DataONE Subscription Plan - Individual',
+    'service',
+    'subscription',
+    'https://dataone.org/products-and-services/individual-subscription',
+    '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "branded_portal_count","softLimit": "1","hardLimit": "1","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
+    );
+
+
+--- Small Organization Subscription
+INSERT INTO products (object, active, name, caption, description, created,
+    statementDescriptor, type, unitLabel, url, metadata)
+    VALUES (
+    'product',
+    true,
+    120000,
+    'Small Organization ',
+    'Small institutions or groups',
+    'USD',
+    'Create multiple portals for your work and projects. Help others understand and access your data.',
+    'year',
+    now(),
+    'DataONE Subscription Plan - Small Organization',
+    'service',
+    'subscription',
+    'https://dataone.org/products-and-services/organization-subscription',
+    '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "branded_portal_count","softLimit": "5","hardLimit": "5","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
+    );
+
+--- Medium Organization Subscription
+INSERT INTO products (object, active, name, caption, description, created,
+    statementDescriptor, type, unitLabel, url, metadata)
+    VALUES (
+    'product',
+    true,
+    600000,
+    'Organization',
+    'Medium institutions or groups',
+    'USD',
+    'Create multiple portals for your work and projects. Help others understand and access your data.',
+    'year',
+    now(),
+    'DataONE Subscription Plan - Medium Organization',
+    'service',
+    'subscription',
+    'https://dataone.org/products-and-services/organization-subscription',
+    '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "branded_portal_count","softLimit": "5","hardLimit": "5","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
+    );
+
+--- Large Organization Subscription
+INSERT INTO products (object, active, name, caption, description, created,
+    statementDescriptor, type, unitLabel, url, metadata)
+    VALUES (
+    'product',
+    true,
+    3600000,
+    'Organization',
+    'Large institutions or groups',
+    'USD',
+    'Create multiple portals for your work and projects. Help others understand and access your data.',
+    'year',
+    now(),
+    'DataONE Subscription Plan - Large Organization',
+    'service',
+    'subscription',
+    'https://dataone.org/products-and-services/organization-subscription',
+    '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "branded_portal_count","softLimit": "5","hardLimit": "5","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
+    );
+
+--- TODO: Hosted Repository
+
+--- TODO: Archival Storage
+
+--- TODO: Consultation
