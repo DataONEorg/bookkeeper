e17ad85b80def9f78ae4a5d31f356023d05c5eb7
Christopher Jones <csj@christopherjones.co>
2019-12-28 13:37:54 -0700

    Add a BaseResource for all resources with a validator if needed.

---
+++ src/main/java/org/dataone/bookkeeper/resources/BaseResource.java
@@ -0,0 +1,38 @@
+/*
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright 2019
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.dataone.bookkeeper.resources;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+/**
+ * A base resource class providing functionality across resource classes
+ */
+class BaseResource {
+    /* The product validator */
+    static Validator validator;
+
+    /* Create a static validator for resources */
+    static {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+}
