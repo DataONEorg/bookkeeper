919b75cb2cfb311d54d89829993381e4838e30a0
Christopher Jones <csj@christopherjones.co>
2019-12-30 14:22:12 -0700

    Fix a repo storage unit copy/paste typo.

--- src/main/resources/db/migrations/V1.2__Insert_Standard_Quotas.sql
+++ src/main/resources/db/migrations/V1.2__Insert_Standard_Quotas.sql
@@ -4,10 +4,9 @@
 INSERT INTO quotas (object, name, softLimit, hardLimit, usage, unit, subscriptionId, subject)
     VALUES ('quota', 'storage_quota', 1024, 1126, NULL, 'megabyte', NULL, NULL);
 
-
 -- 1TB base hosted repository storage limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, usage, unit, subscriptionId, subject)
-    VALUES ('quota', 'repository_storage_quota', 1048576, 1153434, NULL, 'portal_count', NULL, NULL);
+    VALUES ('quota', 'repository_storage_quota', 1048576, 1153434, NULL, 'megabyte', NULL, NULL);
 
 -- Single base branded portal limit
 INSERT INTO quotas (object, name, softLimit, hardLimit, usage, unit, subscriptionId, subject)
