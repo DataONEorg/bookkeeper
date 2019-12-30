ba6650f07dbe94d4949c1df8926b89b7c31a1191
Christopher Jones <csj@christopherjones.co>
2019-12-30 15:22:59 -0700

    Update the products to reflect recent pricing and copy decisions.

--- src/main/resources/db/migrations/V1.8__Insert_Standard_Products.sql
+++ src/main/resources/db/migrations/V1.8__Insert_Standard_Products.sql
@@ -6,7 +6,7 @@ INSERT INTO products (object, active, amount, caption, created, currency, descri
 VALUES (
     'product',
     true,
-    30000,
+    32000,
     'Faculty or research lab',
     now(),
     'USD',
@@ -16,7 +16,7 @@ VALUES (
     'DataONE Subscription Plan - Individual',
     'service',
     'subscription',
-    'https://dataone.org/products-and-services/individual-subscription',
+    'https://products.dataone.org/plus',
     '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "portal_count","softLimit": "1","hardLimit": "1","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
 );
 
@@ -27,7 +27,7 @@ INSERT INTO products (object, active, amount, caption, created, currency, descri
 VALUES (
     'product',
     true,
-    120000,
+    200000,
     'Small institutions or groups',
     now(),
     'USD',
@@ -37,7 +37,7 @@ VALUES (
     'DataONE Subscription Plan - Small Organization',
     'service',
     'subscription',
-    'https://dataone.org/products-and-services/organization-subscription',
+    'https://products.dataone.org/plus',
     '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "portal_count","softLimit": "5","hardLimit": "5","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
 );
 
@@ -57,7 +57,7 @@ VALUES (
     'DataONE Subscription Plan - Medium Organization',
     'service',
     'subscription',
-    'https://dataone.org/products-and-services/organization-subscription',
+    'https://products.dataone.org/plus',
     '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "portal_count","softLimit": "15","hardLimit": "15","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
 );
 
@@ -67,7 +67,7 @@ INSERT INTO products (object, active, amount, caption, created, currency, descri
 VALUES (
     'product',
     true,
-    3600000,
+    2800000,
     'Large institutions or groups',
     now(),
     'USD',
@@ -77,12 +77,45 @@ VALUES (
     'DataONE Subscription Plan - Large Organization',
     'service',
     'subscription',
-    'https://dataone.org/products-and-services/organization-subscription',
+    'https://products.dataone.org/plus',
     '{"features": [{"name": "branded_portal","label": "Branded Portals","description": "Showcase your research, data, results, and usage metrics by building a custom web portal.","quota": {"object": "quota","name": "branded_portal_count","softLimit": "50","hardLimit": "50","unit": "portal"}},{"name": "custom_search_filters","label": "Custom Search Filters","description": "Create custom search filters in your portal to allow scientists to search your holdings using filters appropriate to your field of science."},{"name": "fair_data_assessment","label": "FAIR Data Assessments","description": "Access quality metric reports using the FAIR data suite of checks."},{"name": "custom_quality_service","label": "Custom Quality Metrics","description": "Create a suite of custom quality metadata checks specific to your datasets."},{"name": "aggregated_metrics","label": "Aggregated Metrics","description": "Access and share reports on aggregated usage metrics such as dataset views, data downloads, and dataset citations."},{"name": "dataone_voting_member","label": "DataONE Voting Member","description": "Vote on the direction and priorities at DataONE Community meetings."}]}'
 );
 
---- TODO: Hosted Repository
+-- Hosted Repository
+INSERT INTO products (object, active, amount, caption, created, currency, description,
+    interval, name, statementDescriptor, type, unitLabel, url, metadata)
+VALUES (
+    'product',
+    'true',
+    1200000,
+    'A dedicated repository solution for your group or institution’s data, managed by DataONE',
+    now(),
+    'USD',
+    'Bring the products of your research lab, field station, or library together in a centralized location for efficient sharing, access, and reuse. Accelerate research activity, foster new collaborations, and build community with a repository that meets your needs and reflects your brand.',
+    'year',
+    'DataONE Hosted Repository',
+    'service',
+    'repository',
+    'https://products.dataone.org/hostedrepo',
+    '{"features": [{"name": "trusted_data_repository","label": "A Trusted Data Repository","description": "Preserve and share your data, software, and derived products in a dedicated repository system. Built on our robust and expertly managed repository software and hardware, your research products are safely archived and easily accessible."},{"name": "individual_fair_Assessments","label": "Individual FAIR Assessments","description": "Evaluate your metadata with community established FAIR principles. Scores are refreshed with updates to your metadata, helping make your data even more Findable, Accessible, Interoperable, and Reusable."},{"name": "powerful_online_submission","label": "Powerful Online Submission","description": "The user friendly data submission tool helps your researchers efficiently upload and describe their data. Users can easily create detailed metadata to enhance interoperability, reusability, and value of data."},{"name": "comprehensive_search","label": "Comprehensive Search","description": "Quickly find data with detailed search filters, or by navigating the interactive map."},{"name": "usage_metrics","label": "Usage Metrics","description": "Understand how your data are being used over time with view, download, and citation metrics."},{"name": "usage_metrics","label": "Usage Metrics","description": "Understand how your data are being used over time with view, download, and citation metrics."},{"name": "expandable_storage","label": "Expandable Storage","description": "Grow your repository capacity based on your storage needs in 1 TB increments.","quota": {"object": "quota","name": "repository_storage_quota","softLimit": 1048576,"hardLimit": 1153434,"unit": "portal"}},{"name": "geographic_replicas","label": "Geographic Replicas","description": "Your data are replicated to distinct geographic regions for high availability and preservation."},{"name": "api_access","label": "API Access","description": "Programmatically work with your repository through the DataONE tools in R, Python, Matlab, and Java."},{"name": "link_data_and_software","label": "Link data and Software","description": "Easily show how your files relate to each other by providing well-described provenance workflows."},{"name": "private_and_public_access","label": "Private and Public Access","description": "Control access to your datasets\nprior to publication with private groups or just yourself."},{"name": "private_and_public_access","label": "Private and Public Access","description": "Control access to your datasets\nprior to publication with private groups or just yourself."},{"name": "any_file_format","label": "Any File Format","description": "Use the scientific file formats for your community: image, tabular, text, audio, video, and others."},{"name": "share_when_ready","label": "Share When Ready","description": "Keep your dataset private while you document it thoroughly, and then make it public when you are ready."}]}'
+);
 
---- TODO: Archival Storage
+-- Archival Storage
+INSERT INTO products (object, active, amount, caption, created, currency, description,
+    interval, name, statementDescriptor, type, unitLabel, url, metadata)
+VALUES (
+    'product',
+    'true',
+    126000,
+    'Additional High Availability Data Storage',
+    now(),
+    'USD',
+    'Increase the available storage in your dedicated hosted repository in 1 TB increments.',
+    'year',
+    'Additional Data Storage',
+    'service',
+    'storage',
+    'https://products.dataone.org/hostedrepo',
+    '{}'
+);
 
---- TODO: Consultation
