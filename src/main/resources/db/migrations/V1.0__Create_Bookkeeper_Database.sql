3f95164c38cc555609cf04e304f7ae41612152de
Christopher Jones <csj@christopherjones.co>
2019-12-30 14:17:33 -0700

    Don't create the database - it should be created.

--- src/main/resources/db/migrations/V1.0__Create_Bookkeeper_Database.sql
+++ src/main/resources/db/migrations/V1.0__Create_Bookkeeper_Database.sql
@@ -1,4 +1,4 @@
 --- Note that this is completed in k8s via the Dockerfile
 --- or via s shell script in non-k8s development envs
 --- Create the Bookkeeper database
-CREATE DATABASE bookkeeper WITH ENCODING = 'UTF8';
+--- CREATE DATABASE bookkeeper WITH ENCODING = 'UTF8';
