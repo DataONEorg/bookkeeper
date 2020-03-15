8c60b75ac9f60700fe2eb200f074acf792b12106
Christopher Jones <csj@christopherjones.co>
2020-03-15 15:04:52 -0600

    Add a SQL migration script to create the usages table.

---
+++ src/main/resources/db/migrations/V1.12__Create_Usages_Table.sql
@@ -0,0 +1,24 @@
+--- Note that this is completed in k8s via the Dockerfile
+--- or via a shell script in non-k8s development envs
+
+--- Create the usages table
+CREATE SEQUENCE IF NOT EXISTS usages_id_seq;
+
+CREATE TABLE IF NOT EXISTS usages (
+    id integer DEFAULT nextval('usages_id_seq') PRIMARY KEY,
+    object text NOT NULL,
+    quotaId integer NOT NULL
+    instanceId text NOT NULL
+    quantity real NOT NULL
+);
+ALTER SEQUENCE usages_id_seq OWNED BY usages.id;
+
+CREATE INDEX usages_quotaId_idx ON usages USING btree(quotaId);
+CREATE INDEX usages_instanceId_idx ON usages USING btree(instanceId);
+
+COMMENT ON TABLE  usages IS 'Usages records each instance a portion of a quota is used.';
+COMMENT ON COLUMN usages.id IS 'The unique usage identifier.';
+COMMENT ON COLUMN usages.object IS 'The serialized object type, set to "usage".';
+COMMENT ON COLUMN usages.quotaId IS 'The quota identifier.';
+COMMENT ON COLUMN usages.instanceId IS 'The instance identifier using a portion of the quota.';
+COMMENT ON COLUMN usages.quantity IS 'The quantity used by the instance, in the quota\'s units.';
