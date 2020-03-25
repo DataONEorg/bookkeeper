1cd3e232c0eca7fe77263999a83ef6557c7500d2
Christopher Jones <csj@christopherjones.co>
2020-03-25 15:30:10 -0600

    Add a SQL trigger to update quota usages:
    When the usages table is modified via an insert, update, or
    delete operation, re-tally the usages for the given
    quotaId and update the quota table usage field.

---
+++ src/main/resources/db/migrations/V1.13__Create_Function_Update_Quota_Table_Usage.sql
@@ -0,0 +1,52 @@
+--- Note that this is completed in k8s via the Dockerfile
+--- or via a shell script in non-k8s development envs
+
+--- Create the update quota usage function for inserts and updates
+CREATE OR REPLACE FUNCTION update_quota_usage_on_insert_or_update()
+    RETURNS trigger
+    AS $update_quota_usage_on_insert_or_update$
+    BEGIN
+        -- Check that quotaId is given
+        IF NEW.quotaId IS NULL THEN
+            RAISE EXCEPTION 'quotaId cannot be null';
+        END IF;
+
+        -- Update the quotas.usage column
+        UPDATE quotas
+            SET usage = (SELECT SUM(u.quantity) FROM usages u WHERE quotas.id = NEW.quotaId)
+            WHERE quotas.id = NEW.quotaId;
+        RETURN NEW;
+    END;
+    $update_quota_usage_on_insert_or_update$
+    LANGUAGE plpgsql;
+
+--- Create the update quota usage trigger
+DROP TRIGGER IF EXISTS update_quotas_usage_on_insert_or_update ON usages;
+CREATE TRIGGER update_quotas_usage_on_insert_or_update AFTER INSERT OR UPDATE ON usages
+    FOR EACH ROW
+        EXECUTE PROCEDURE update_quota_usage_on_insert_or_update();
+
+--- Create the update quota usage function for deletes
+CREATE OR REPLACE FUNCTION update_quota_usage_on_delete()
+    RETURNS trigger
+    AS $update_quota_usage_on_delete$
+    BEGIN
+        -- Check that quotaId is given
+        IF OLD.quotaId IS NULL THEN
+            RAISE EXCEPTION 'quotaId cannot be null';
+        END IF;
+
+        -- Update the quotas.usage column
+        UPDATE quotas
+            SET usage = (SELECT SUM(u.quantity) FROM usages u WHERE quotas.id = OLD.quotaId)
+            WHERE quotas.id = OLD.quotaId;
+        RETURN OLD;
+    END;
+    $update_quota_usage_on_delete$
+    LANGUAGE plpgsql;
+
+--- Create the update quota usage trigger
+DROP TRIGGER IF EXISTS update_quotas_usage_on_delete ON usages;
+CREATE TRIGGER update_quotas_usage_on_delete AFTER DELETE ON usages
+    FOR EACH ROW
+        EXECUTE PROCEDURE update_quota_usage_on_delete();
