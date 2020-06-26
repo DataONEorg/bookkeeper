40e482b6cb0cca623ec7250f5a26cac1faa1dd47
gothub <slaughter@nceas.ucsb.edu>
2020-06-26 13:11:14 -0700

    Add'l db column renaming (#14)

--- src/main/java/org/dataone/bookkeeper/jdbi/SubscriptionStore.java
+++ src/main/java/org/dataone/bookkeeper/jdbi/SubscriptionStore.java
@@ -197,9 +197,9 @@ public interface SubscriptionStore {
      * @param quota the quota to insert
      */
     @SqlUpdate("INSERT INTO quotas " +
-        "(object, name, softLimit, hardLimit, usage, unit, subscriptionId, subject) " +
+        "(object, quotaType, softLimit, hardLimit, usage, unit, subscriptionId, subject) " +
         "VALUES " +
-        "(:object, :name, :softLimit, :hardLimit, :usage, :unit, :subscriptionId, :subject) " +
+        "(:object, :quotaType, :softLimit, :hardLimit, :usage, :unit, :subscriptionId, :subject) " +
         "RETURNING id")
     @GetGeneratedKeys
     Integer insertQuota(@BindBean Quota quota);
