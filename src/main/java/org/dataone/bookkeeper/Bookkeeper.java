dad47f6e4a5b5ae93911bb0712be955748850c3c
Christopher Jones <csj@christopherjones.co>
2020-10-07 12:44:36 -0600

    Remove the call to register the MembershipResource.
    refs https://github.com/DataONEorg/bookkeeper/issues/65

--- src/main/java/org/dataone/bookkeeper/Bookkeeper.java
+++ src/main/java/org/dataone/bookkeeper/Bookkeeper.java
@@ -108,9 +108,6 @@ public class Bookkeeper extends Application<BookkeeperConfiguration> {
         // Register the orders resource
         environment.jersey().register(new OrdersResource(database, dataoneHelper));
 
-        // Register the memberships resource
-        environment.jersey().register(new MembershipsResource(database, dataoneHelper));
-
         // Register role-based authorization
         environment.jersey().register(RolesAllowedDynamicFeature.class);
 
